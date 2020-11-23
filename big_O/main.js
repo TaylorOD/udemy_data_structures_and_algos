d4ebdeee33d31ae428699fa43162df78bda9064d
taylorod <taylorsunwantedemails@gmail.com>
2020-11-23 11:24:39 -0600

    add first function

--- big_O/main.js
+++ big_O/main.js
@@ -1,5 +1,11 @@
 const nemo = ["nemo"]
 
 function findNemo(array) {
-  for ()
+  for (let i = 0; i < array.length; i += 1) {
+    if(array[i] === "nemo") {
+      console.log("Found NEMO!")
+    }
+  }
 }
+
+findNemo(nemo)
