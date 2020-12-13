995389a411ac31a50b9d857ee82a239511a6b230
taylorod <taylorsunwantedemails@gmail.com>
2020-12-13 13:28:34 -0600

    add second folder

---
+++ 2_how_to_solve/common_array.js
@@ -0,0 +1,5 @@
+// compare two arrays - return true if any item in the first array is the same as any item in the second array
+
+const array1 = ["a", "b", "c", "x"]
+const array2 = ["z", "y", "a"]
+
